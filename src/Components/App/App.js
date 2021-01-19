ee67829df8578ba13cf77b07e841b4fcc70c96cf
AlDiAlisel <alessio.farroni@gmail.com>
2021-01-19 17:45:30 +0100

    first commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,62 +1,124 @@
-import './App.css';
+
+import { useState, useEffect} from 'react'
 import Weather from '../Weather/Weather'
-import Form from '../Form/Form'
-import {useState} from 'react'
+import TextField from '@material-ui/core/TextField';
+import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import Switch from '@material-ui/core/Switch';
+import './App.css';
+import github from '../../github.png';
+import linkedin from '../../linkedin.png'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '29ch',
+      textAlign: 'center',
+      verticalAlign: 'super',
+     
+    },
+  },
+  button:{
+    textAlign:'buttom',
+  },
+  switch :{
+    width:'60px',
+  }
+}));
 
 function App() {
-  const [weather,setWeather] = useState([])
+ const [valoreInput, setValoreInput]= useState ('')
+ const [city, setCity] = useState('')
+ const APIKEY = 'd1a0e9818bd599a5fd37abd9151a7791';
+ const form = useStyles();
+ const[state, setState]=useState({checkedA:false})
+ const [tempo, setTempo] = useState({
+     description:'',
+     temp:'',
+     humidity:'',
+     vero: true,
+     main:''
+ })
 
-  const APIKEY = 'd0d5c38bcf3d13d80ce73297fdec62b5'
 
-  async function fetchData(event) {
-    const city = event.target.element.city.value
-      e.preventDefault()
-    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKEY}`)
-      .then( res => res.json())
-      .then(data => data)
-      if(city) {
-      setWeather({
-        data: apiData,
-        city: apiData.city,
-        description: apiData.weather[0].description,
-        temperature: Math.round(apiData.main.temp - 273.15),
-        humidity: apiData.main.humidity,
-        error:""
-      }
-      )} else {
-        setWeather({
-          data: '',
-          city: '',
-          description: '',
-          temperature: '',
-          humidity: '',
-          error:"Scrivi una città"
-      }
-      )}
-  }
+  function ip(event){
+    if(event.target !== undefined){
+    setState({ ...state, [event.target.name]: event.target.checked });
+    axios.get('https://api.astroip.co/5.61.219.180?api_key=3771004b-1944-43a8-aa17-3868279843d7')
+    .then(response =>{
+    const  clientIp = response.data;
+    setCity(clientIp.geo.city);
+      })
+    }
+ }
+ 
 
-  return (
-    <div className="app">
-      <div className="app-container">
-        <div className="form">
-        <h2>app</h2>
-        <Form getWeather={fetchData} />
-        </div>
-  
-        <Weather
-          city={weather.city}
-          description={weather.description}
-          temperature={weather.temperature}
-          humidity={weather.humidity}
-          error={weather.error}
-        />
+useEffect(()=>{
+ axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKEY}`)
+ .then(response => {
+  const weather = response.data;
+  setTempo({
+    description: weather.weather[0].description,
+    temp:Math.floor(weather.main.temp - 273.15),
+    humidity:weather.main.humidity,
+    vero:  true ? false: true,
+    main:weather.weather[0].main
+  })
+ }, error => {
+  console.log(error);
+ });
+ 
+ 
+},[city, tempo.vero]);
 
-        {/* <FotecastWeather /> */}
 
+const handleChange = event => {
+ setValoreInput(event.target.value);
+}
+
+function invia (){
+    setCity(valoreInput); 
+}
+
+ return (
+  <div className="App">
+     <div className='header'>Developed by Alessio Farroni</div>
+     <div className="bg"></div>
+     <div className="bg bg2"></div>
+     <div className="bg bg3"></div>
+     <div className="app-input">
+      <div className="content">
+      <h1>Weather forecasts</h1>
+      <form className={form.root} noValidate autoComplete="off">
+      <span id='switchIp'>Serch by ip</span>
+      <Switch 
+        className={form.switch}
+        checked={state.checkedA}
+        onChange={ip}
+        name="checkedA"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      <TextField id="standard-basic" label="weather forecasts, type your city" city = {city} onChange={handleChange}/>
+      <Button className={form.button} color="primary" onClick={invia} >Search</Button>
+      </form>
+      <div className="app-weather">
+    
+      { tempo.main === ''? console.log('') : <Weather
+      city={city}
+      description={tempo.description}
+      temp={tempo.temp}
+      humidity={tempo.humidity}
+      main={tempo.main}
+      />   }
       </div>
-      
     </div>
-  );
+   
+  </div>
+  <footer className='footer'><div><a href='https://github.com/AlDiAlisel' className='icons' ><img src={github} /></a><a href='' className='icons'><img src={linkedin}/></a></div></footer>
+  </div>
+ )
 }
 
 export default App;
--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,16 +1,109 @@
-import React from 'react'
+
+import { useState, useEffect} from 'react'
+import {Grid,  makeStyles, Card, CardActionArea,CardActions, CardContent, CardMedia, Typography,Switch} from '@material-ui/core';
+import axios from 'axios';
+import './Weather.css'
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 400,
+    },
+    media :{
+      width: 230,
+    }
+  });
 
 
-const Weather = ({description, city, error, temperature, humidity}) => {
+ 
+function Weatherr(props) {
+    const uri =`https://translation.googleapis.com/language/translate/v2`;
+    const {temp, humidity,city,description,main} = props;
+    const [state, setState]=useState({'checkedA':false})
+    const [traduzione, setTraduzione]=useState();
+    const [info, setInfo]=useState([]);
+    const[infoEn, setInfoEn]=useState(['the weather is :', ' the temperature is :', 'humidity  :']);
+    const[img, setImg]=useState();
+    const card= useStyles();
+    
+     const handleChange = (event) => {
+        setState({...state,[event.target.name] :event.target.checked});
+        axios.post(
+            uri,
+            {},
+            {
+              params: {
+                q:  infoEn[0] + infoEn[1] + infoEn[2] + description,
+                target: 'it',
+                key: process.env.REACT_APP_API_KEY
+              }
+            }
+          )
+        .then(response =>{
+            const x = response.data.data.translations[0].translatedText
+            let affermazioni = x.split(' ');
+            let a =affermazioni.slice(0,2);
+            let b = affermazioni.slice(3,6);
+            let c=affermazioni.slice(6,7);
+            let d = affermazioni.slice(7)
+            setInfo([a.map(x=> ' ' + x),b.map(x => ' ' + x), c.map(x => ' ' + x )])
+            setTraduzione(d.map(x => ' ' + x));
+        })
+      };
+
+   useEffect(()=>{
+    if(main === 'Mist'){
+      setImg('https://media.giphy.com/media/Xev6MDHoxXrUAQZDMa/giphy.gif')
+     } if(main==='Thunderstorm'){
+         setImg('https://media.giphy.com/media/Q8bCsT2A4cuVM8C93X/giphy.gif')
+     }if(main==='	Drizzle'){
+       setImg('https://media.giphy.com/media/SVqwRUVQqYDz9wHCHF/giphy.gif')
+     }if(main==='Rain'){
+       setImg('https://media.giphy.com/media/3ov9jCEFMBtCy54q6Q/giphy.gif')
+     }if(main==='Snow'){
+       setImg('https://media.giphy.com/media/3iCuUglCmyJSTQ1l3w/giphy.gif')
+     }if(main==='Fog' || main==='Smoke'){
+       setImg('https://media.giphy.com/media/ejB3NVGzbPKTUONSN3/giphy.gif')
+     }if(main==='Clear'){
+       setImg('https://media.giphy.com/media/UnyblOs6hGx9Mli7jq/giphy.gif')
+     }if(main==='Clouds'){
+       setImg('https://media.giphy.com/media/VDMgMo6U4LFuioPGFn/giphy.gif')
+     }
+   },[main])
+       
+  
+    
  return (
-     <div className="weather">
-         {city && <p>{city}</p>}
-         {temperature && <p>{temperature} C</p>}
-         {description && <p> {description}</p>}
-         {humidity && <p>{humidity}% humidity </p> }
-         {error && <p>{error}</p>}
-     </div>
+    <Grid   container direction='column' justify='flex-start' alignItems='center'>
+      <Card className={card.root}  >
+       <CardActionArea>
+          <CardMedia
+          className={card.media}
+          component="img"
+          alt="Weather"
+          image={img}
+          title="Weather"
+         
+         />
+          <CardContent>
+           <Typography gutterBottom variant="h5" component="h2">{city}</Typography>
+           <Typography variant="body2" color="textSecondary" component="p">{state.checkedA && info !== undefined? info[0]: infoEn[0]}  {state.checkedA? traduzione: description}</Typography>
+           <Typography>{state.checkedA && info !== undefined? info[1]: infoEn[1] }{temp + ' °C'}</Typography>
+           <Typography>{state.checkedA && info !== undefined ? info[2] : infoEn[2] } {humidity + ' %'}</Typography>
+         </CardContent>
+       </CardActionArea>
+          <CardActions>
+          English<Switch
+          onChange={handleChange}
+          checked={state.checkedA} 
+          name="checkedA"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+          />Italian
+      
+          </CardActions>
+      </Card> 
+    </Grid>
  )
 }
 
-export default Weather; 
+export default Weatherr
+
